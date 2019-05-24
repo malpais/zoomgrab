326564b4498b9a89dec55fad4c3e04eab26aa807
Steve Coward <steve.coward@gmail.com>
2019-05-24 10:49:41 -0400

    adds comments back to proxies declaration

--- zoomgrab.py
+++ zoomgrab.py
@@ -18,8 +18,8 @@ headers = {
 }
 
 proxies = {
-    'http': 'http://127.0.0.1:8080',
-    'https': 'http://127.0.0.1:8080',
+    # 'http': 'http://127.0.0.1:8080',
+    # 'https': 'http://127.0.0.1:8080',
 }
 
 banner = """
